2d24182777b6373bd733ab7899c4de30763c24be
asthasingh011 <as7581@srmist.edu.in>
2026-06-14 00:46:34 +0530

    Connected backend to Railway database

--- backend/server.js
+++ backend/server.js
@@ -8,13 +8,14 @@ app.use(cors());
 app.use(express.json());
 
 // 🔗 DB CONNECTION
-require("dotenv").config();
+require('dotenv').config();
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 db.connect(err => {
